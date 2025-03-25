67f903b6d5f31bcf4ec2f9b057a688430cc2cf04
Hamza-Bouali <hamza.bouali@esi.ac.ma>
2025-03-25 19:52:27 +0000

    afafafrwa

--- src/App.tsx
+++ src/App.tsx
@@ -70,15 +70,15 @@ const HackathonWebsite = () => {
       </section>
       {/* Sponsors Section */}
 
-      <section className="py-16 px-4 bg-gradient-to-b from-[#010084]/50 to-[#220066]/80" id="sponsors">
+      {<section className="py-16 px-4 bg-gradient-to-b from-[#010084]/50 to-[#220066]/80" id="sponsors">
         <div className="container mx-auto max-w-6xl">
           
-          <h2 className="text-8xl md:text-6xl font-bold text-center mb-16">
+          {/*<h2 className="text-8xl md:text-6xl font-bold text-center mb-16">
             <span className="bg-gradient-to-r from-[#00CFFF] to-[#C4E2ED] text-transparent bg-clip-text">
               Our Sponsors
             </span>
-          </h2>
-            {sponsors.length > 0 && (
+          </h2>*/}
+            {/*sponsors.length > 0 && (
             <div className="mb-20">
               <h3 className="text-2xl font-bold text-center mb-10 text-[#00CFFF]">
               
@@ -95,22 +95,13 @@ const HackathonWebsite = () => {
                   </a>
                   <p className="mb-4 font-normal text-gray-400 hover:text-gray-300 transition-colors duration-300">
                     {sponsor.text}
-                  </p>{/*
-                  <a
-                    href="#"
-                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                  >
-                    Read more
-                    <svg className="rtl:rotate-180 w-4 h-4 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                  </a>*/}
+                  </p>
                   </div>
                 </div>
               ))}
               </div>
             </div>
-            )}
+            )*/}
 
 
           {/* Platinum Sponsors */}
@@ -220,7 +211,7 @@ const HackathonWebsite = () => {
           {/* Become a Sponsor CTA */}
           
         </div>
-      </section>
+      </section>}
 
       {/* About Section */}
       <section className="py-16 px-4 bg-[#010084]/30 backdrop-blur-sm" id="about">
